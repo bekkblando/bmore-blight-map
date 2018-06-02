e48270e50028ae702c2b6df0d0c9b96e3f9e5e1a
bekk <bekkblando@gmail.com>
2018-06-02 05:59:50 -0400

    working on date time

--- bmore.js
+++ bmore.js
@@ -12,13 +12,72 @@ d3.json("bmore.json", function(error, bmore) {
       .datum(neighborhoods)
       .attr("d", path);
 
-  d3.json("stuff.json", function(error, stuff){
-    if (error) throw error;
-    var svg = d3.select("svg");
-    svg.append("path")
-    .datum(stuff)
-    .attr("d", path)
-    .attr("class", "place");
-
-  });
 });
+
+$(function() {
+  $('input[name="datetimes"]').daterangepicker({
+    startDate: moment().startOf('hour'),
+    endDate: moment().startOf('hour').add(32, 'hour'),
+    showDropdowns: true,
+    locale: {
+      format: 'M/DD hh:mm A'
+    }
+  })
+  $('input[name="datetimes"]').on('apply.daterangepicker',
+    (event, obj) => {
+
+      $.ajax({
+          url: "https://data.baltimorecity.gov/resource/9t78-k3wf.geojson",
+          type: "GET",
+          data: {
+            "$$app_token" : "eBYEhO8U5MV3A40adxqkH4JRq"
+          }
+      }).done(function(data) {
+        console.log(data)
+        let destructionFiltered = {
+          "type": "FeatureCollection",
+          "features":  getDataPoints(obj.startDate, obj.endDate, data, "dateissue")
+        };
+        console.log("Destruction ", destructionFiltered)
+        d3.select("svg").append("path")
+       .datum(destructionFiltered)
+       .attr("d", path)
+       .attr("class", "destruct")
+       .style("fill", "red")
+
+      });
+
+      $.ajax({
+          url: "https://data.baltimorecity.gov/resource/rw5h-nvv4.geojson",
+          type: "GET",
+          data: {
+            "$$app_token" : "eBYEhO8U5MV3A40adxqkH4JRq"
+          }
+      }).done(function(data) {
+          let vacencyFiltered = {
+            "type": "FeatureCollection",
+            "features": getDataPoints(obj.startDate, obj.endDate, data, "noticedate")
+          };
+
+          d3.select("svg").append("path")
+         .datum(vacencyFiltered)
+         .attr("d", path)
+         .attr("class", "vacency")
+         .style("fill", "blue")
+      });
+
+    });
+});
+
+function getDateFromElement(element, dateKey) {
+  return new Date(element['properties'][dateKey]);
+}
+
+function getDataPoints(start, end, data, dateKey) {
+  return data['features'].filter(datum => {
+    const datumDate = getDateFromElement(datum, dateKey);
+    return (start < datumDate && datumDate < end);
+  }).sort((a, b) => {
+    return getDateFromElement(b, dateKey) - getDateFromElement(a, dateKey);
+    });
+}
